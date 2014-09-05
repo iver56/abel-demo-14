60e18704e6d18a6829ebcac320fd022d52a6a4f5
Stian Jensen <me@stianj.com>
2014-09-05 19:14:44 +0200

    Add shoutouts (rushed for music)

--- src/ShoutOutLayer.js
+++ src/ShoutOutLayer.js
@@ -3,22 +3,61 @@
  */
 function ShoutOutLayer(layer) {
   this.cubes = [];
-  this.numCubes = 7;
+  this.second_cubes = [];
+  this.numCubes = 10;
 
   this.scene = new THREE.Scene();
 
   this.cameraController = new CameraController(layer.position);
   this.camera = this.cameraController.camera;
 
-  var cubeGeometry = new THREE.BoxGeometry(50, 50, 50);
-  var cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff9900});
+  this.cubeGeometry = new THREE.BoxGeometry(50, 50, 50);
 
+  var letters = [
+    ['-', '-', 'e', '-', '-', 'o'],
+    ['a', '-', 'm', '-', 's', 'l'],
+    ['l', 'a', 'm', '-', 'i', 'o'],
+    ['e', 'e', 'a', '-', 'g', 'l'],
+    ['k', 't', '-', '-', 'v', 'o'],
+    ['s', 's', 'e', '-', 'e', 'r'],
+    ['a', 'i', 'm', '-', 's', 'c'],
+    ['n', 'r', 'i', '-', 'e', 's'],
+    ['b', 'c', 'l', '-', 'b', 'r'],
+    ['-', '-', 'e', '-', '-', 'm']
+  ];
   for (var i = 0; i < this.numCubes; i++) {
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(150 * i, -50 * i, -50 * i);
+    var cube = this.createCube(letters[i]);
+    cube.position.set(70 * i - 400, 0, -70 * i + 400);
     this.cubes.push(cube);
     this.scene.add(cube);
   }
+  Loader.start(function(){}, function(){});
+
+  var second_letters = [
+    ['-', '-', 'e', '-', '-', '-'],
+    ['-', '-', 'm', '-', 'l', 'b'],
+    ['p', '-', 'm', '-', 'i', 'o'],
+    ['r', '-', 'a', '-', 'o', 'o'],
+    ['o', 'r', '-', '-', 'n', 'd'],
+    ['f', 'u', 'e', '-', 'l', '-'],
+    ['i', 'n', 'm', '-', 'e', 'r'],
+    ['t', '-', 'i', '-', 'a', 'm'],
+    ['-', '-', 'l', '-', 'f', '-'],
+    ['-', '-', 'e', '-', '-', '-']
+  ];
+  for (var i = 0; i < this.numCubes; i++) {
+    var cube = this.createCube(second_letters[i]);
+    cube.position.set(70 * i - 400, 0, -70 * i + 400);
+    this.second_cubes.push(cube);
+  }
+  Loader.start(function(){}, function(){});
+
+  var planeGeo = new THREE.PlaneGeometry(5000, 5000);
+  var planeMaterial = new THREE.MeshBasicMaterial({color: 0x55ff55, side: THREE.BackSide});
+  this.plane = new THREE.Mesh(planeGeo, planeMaterial);
+  this.plane.rotation.x = Math.PI / 2;
+  this.plane.position.setY(-25);
+  this.scene.add(this.plane);
 
   var light = new THREE.PointLight( 0xffffff, 1, 100 );
   light.position.set(0, 0, 300);
@@ -37,4 +76,33 @@ ShoutOutLayer.prototype.render = function(renderer, interpolation) {
 
 ShoutOutLayer.prototype.update = function(frame, relativeFrame) {
   this.cameraController.updateCamera(relativeFrame);
+
+  if (relativeFrame >= 451 && !this.secondCubesInScene) {
+    this.secondCubesInScene = true;
+    for (var i=0; i < this.numCubes; i++) {
+      this.scene.remove(this.cubes[i]);
+      this.scene.add(this.second_cubes[i]);
+    }
+  } else if (relativeFrame <= 450 && this.secondCubesInScene) {
+    this.secondCubesInScene = false;
+    for (var i=0; i < this.numCubes; i++) {
+      this.scene.remove(this.second_cubes[i]);
+      this.scene.add(this.cubes[i]);
+    }
+  }
+};
+
+ShoutOutLayer.prototype.createCube = function(letters) {
+  var imageSuffix = ".png";
+
+  var materialArray = [];
+  for (var i = 0; i < 6; i++) {
+    materialArray.push(new THREE.MeshBasicMaterial({
+      map: Loader.loadTexture('res/textures/cube_' + letters[i] + imageSuffix),
+      side: THREE.FrontSide
+    }));
+  }
+  var cubeMaterial = new THREE.MeshFaceMaterial(materialArray);
+  var cube = new THREE.Mesh(this.cubeGeometry, cubeMaterial);
+  return cube
 };
