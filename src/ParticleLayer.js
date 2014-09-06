b04036a60f819f8993aa9df17bd03400fd3faf92
iver56 <iver56@hotmail.com>
2014-09-06 18:40:34 +0200

    More particles and camera paths

--- src/ParticleLayer.js
+++ src/ParticleLayer.js
@@ -74,14 +74,14 @@ ParticleLayer.prototype.archimedesSpiral = function(from, relativeFrame, to) {
   };
 };
 
-ParticleLayer.prototype.astroidPedalCurve = function(from, relativeFrame, to) {
+ParticleLayer.prototype.astroidPedalCurve = function(from, relativeFrame, to, offset, scale) {
   var relativeRelativeFrame = relativeFrame - from;
   var length = to - from;
-  var t = 12 * clamp(0, relativeRelativeFrame / length, 1);
+  var t = 2.02 * Math.PI * clamp(0, relativeRelativeFrame / length, 1);
   return {
-    x: 400 * Math.cos(t) * Math.sin(t) * Math.sin(t),
-    z: 300,
-    y: 400 * 0.5 * Math.sin(t) * (Math.cos(2 * t) + 1)
+    x: scale * 400 * Math.cos(t) * Math.sin(t) * Math.sin(t) + offset.x,
+    z: scale * 300 + offset.y,
+    y: scale * 400 * 0.5 * Math.sin(t) * (Math.cos(2 * t) + 1) + offset.z
   };
 };
 
@@ -94,7 +94,7 @@ ParticleLayer.prototype.update = function(frame, relativeFrame) {
     pos = this.archimedesSpiral(150, relativeFrame, 500);
 
   } else if (relativeFrame <= 700) {
-    var offset = {x: 85, y: 285, z: 0};
+    var offset = {x: 55, y: 285, z: 0};
     var scale = 2.5;
     pos = this.heart(500, relativeFrame, 700, offset, scale);
 
@@ -107,12 +107,22 @@ ParticleLayer.prototype.update = function(frame, relativeFrame) {
     var offset = {x: 700, y: 250, z: 0};
     var scale = 1;
     pos = this.heart(780, relativeFrame, 860, offset, scale);
+  } else if (relativeFrame <= 1040) {
+    var offset = {x: 0, y: 0, z: 0};
+    var scale = 1;
+    pos = this.astroidPedalCurve(860, relativeFrame, 1040, offset, scale);
 
-  } else if (relativeFrame <= 1200) {
-    pos = this.astroidPedalCurve(860, relativeFrame, 1200);
+  } else if (relativeFrame <= 1240) {
+    var offset = {x: 0, y: 0, z: 0};
+    var scale = 0.8;
+    pos = this.astroidPedalCurve(1040, relativeFrame, 1240, offset, scale);
+
+  } else if (relativeFrame <= 1440) {
+    var offset = {x: 0, y: 0, z: 0};
+    var scale = 0.6;
+    pos = this.astroidPedalCurve(1240, relativeFrame, 1440, offset, scale);
   }
 
-  console.log(pos);
   this.ps.emitterPosition.x = pos.x;
   this.ps.emitterPosition.y = pos.y;
   this.ps.emitterPosition.z = pos.z;
